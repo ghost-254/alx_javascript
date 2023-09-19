e6506769ebc8b58cf7cbbd1b52ce04daf1282b0d
ghost-254 <nevilleotieno6@gmail.com>
2023-09-19 06:01:01 -0700

    Task: 3. Loripsum

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node 3-request_store.js <URL> <file path>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: Status code ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  // Writing the response body to the specified file with UTF-8 encoding
+  fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+    if (writeError) {
+      console.error(`Error writing to file: ${writeError.message}`);
+      process.exit(1);
+    }
+
+    console.log(`Contents of ${url} have been saved to ${filePath}`);
+  });
+});
